b3429019e2db1710d4326567500580903c3c9f84
Alikhan <chort445@gmail.com>
2024-11-06 14:59:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+//Tapsyrma1)
+
+let age = prompt("Enter your age");
+
+if ( age >18) 
+    alert("18 толган")
+else ( age <18) 
+    alert("18 толмаган")
+
+    
+  
+//Tapsyrma2)
+
+let temp = prompt("температура жаз")
+
+if (temp > 25) 
+    alert("жеңіл шорты, футболка")
+else (temp < -0)
+    alert("қалың куртка, жемпір")
+    
